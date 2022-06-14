629a726787b5a728edb5741870fb731443516367
girishdev <girishkumar.aorg@gmail.com>
2022-06-14 19:41:01 +0530

    Added few Imp Links

--- Notes-1.php
+++ Notes-1.php
@@ -17,12 +17,19 @@ but when you Release Specific Version in PS, then you need to create New Migrati
 php artisan migrate:rollback => This will go One Step backwords
 php artisan migrate
 
+IDE Setup:
+    https://www.jetbrains.com/help/phpstorm/2022.1/laravel.html
+    https://www.javatpoint.com/laravel-tinker#:~:text=Laravel%20Tinker%20allows%20you%20to,works%20with%20a%20php%20artisan.
+    https://stillat.com/blog/2016/12/07/laravel-artisan-the-tinker-command
+
 Concept Learning In this Project:
 1. Routes
 2. Controller
 3. Views(Blade File)
 4. HTML forms(Blade)
 5. Validation
+    https://laravel.com/docs/master/validation#available-validation-rules
+
 6. Eloquent
 7. Sending Mails
 8. Flashing Data (Session/Cookies)
